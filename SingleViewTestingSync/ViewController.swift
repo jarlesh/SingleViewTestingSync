2238490411c456f8418839c5b8c4f54511609086
Jarle Heldstab <jarlehs@gmail.com>
2018-01-19 11:19:50 +0100

    Initial Commit

---
+++ SingleViewTestingSync/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SingleViewTestingSync
+//
+//  Created by Jarle Heldstab on 19/01/2018.
+//  Copyright © 2018 Jarle Heldstab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
